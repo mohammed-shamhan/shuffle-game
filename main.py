968715599d9ccc938df832f4f83e0bba309a6b03
arthurm444 <87413150+arthurm444@users.noreply.github.com>
2021-08-26 14:07:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from random import shuffle
+
+my_list = [' ','O',' ']
+def shuffle_list(my_list):
+    shuffle(my_list)
+    return my_list
+shuffle_list(my_list)
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        # Recall input() returns a string
+        guess = input("Pick a number: 0, 1, or 2:  ")
+    return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print('Correct Guess!')
+    else:
+        print('Wrong! Better luck next time')
+        print(mylist)
+
+
+# Initial List
+mylist = [' ','O',' ']
+
+# Shuffle It
+mixedup_list = shuffle_list(mylist)
+
+# Get User's Guess
+guess = player_guess()
+
+# Check User's Guess
+check_guess(mixedup_list,guess)
